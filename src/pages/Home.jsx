5800ab9b64d52c2d3015f2a464579d879378b09c
sethorater <118314693+sethorater@users.noreply.github.com>
2025-12-01 11:49:41 -0700

    seths home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,37 @@
+import { PlayerCard, MatchCard } from '@montana-state-club-soccer/mscss'
+import PlayerImage from '../images/team.jpg';
+
 function Home() {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-6">Welcome to Montana State Club Soccer</h1>
-      <p className="text-gray-600 mb-4">
-        This is the home page. The white background you see is the content area, 
-        and the gray background around it creates the boundary effect.
-      </p>
-      <p className="text-gray-600">
-        All page content will stay within these boundaries, aligned with the header 
-        logo and footer content.
-      </p>
+      <PlayerCard 
+      image={PlayerImage}
+      />
+      <MatchCard className="mt-6"
+        awayTeam="University of Montana"
+        date="2025-02-15"
+        homeTeam="Montana State"
+        location="Lambert Turf Field"
+        status="upcoming"
+        time="19:00"
+      />
+      <MatchCard className="mt-6"
+        awayTeam="University of Utah"
+        date="2025-03-21"
+        homeTeam="Montana State"
+        location="Lambert Turf Field"
+        status="upcoming"
+        time="17:00"
+      />
+      <MatchCard className="mt-6"
+        awayTeam="Gonzaga University"
+        date="2025-04-9"
+        homeTeam="Montana State"
+        location="Lambert Turf Field"
+        status="upcoming"
+        time="20:00"
+      />
     </div>
   )
 }
