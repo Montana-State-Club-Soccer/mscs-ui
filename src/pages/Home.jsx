9e6a832d6f74f9fa3d5994c26b2621ab4d8c6b5f
Seth Keirn <118314693+sethorater@users.noreply.github.com>
2025-12-04 10:51:35 -0700

    feat: panoramic image added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
-import { PlayerCard, MatchCard, TabsList, TabsContent, Tabs, TabsTrigger } from '@montana-state-club-soccer/mscss'
-import PlayerImage from '../images/team.jpg';
+import { MatchCard, TabsList, TabsContent, Tabs, TabsTrigger, ImageBlock, Divider } from '@montana-state-club-soccer/mscss'
+import PlayerImage from '../images/team24.jpg';
 
 function Home() {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-6">Welcome to Montana State Club Soccer</h1>
-      <PlayerCard 
+
+      <ImageBlock 
+      aspectRatio="panoramic" 
       image={PlayerImage}
       />
+
+      <Divider className='mt-6' variant="secondary"/>
+
       <MatchCard className="mt-6"
         awayTeam="University of Montana"
         date="2025-02-15"
@@ -16,6 +21,7 @@ function Home() {
         status="upcoming"
         time="19:00"
       />
+
       <MatchCard className="mt-6"
         awayTeam="University of Utah"
         date="2025-03-21"
@@ -24,6 +30,7 @@ function Home() {
         status="upcoming"
         time="17:00"
       />
+
       <MatchCard className="mt-6"
         awayTeam="Gonzaga University"
         date="2025-04-9"
