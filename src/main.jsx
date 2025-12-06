3275c47bbc675d30964a3f8da1d4056076f77141
Landon Farrar <landon.farrar@yahoo.com>
2025-12-05 20:52:40 -0700

    Use local CSS file instead of npm package import for Vercel build compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'mscss-montana/dist/styles.css'
+import './mscss-styles.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
