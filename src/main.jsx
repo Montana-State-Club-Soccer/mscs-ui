ebc7c5bac8d2de6bc9e34d0c7c9d9b9e7e53ffb5
Landon Farrar <landon.farrar@yahoo.com>
2025-12-05 20:48:20 -0700

    Import MSCSS styles in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'mscss-montana/dist/styles.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
